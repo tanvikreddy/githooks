5538440b3880fec5d0d8dac0116d6679349cca6d
Santhosh Kumar Koppula <skoppula@microsoft.com>
2025-05-08 22:18:35 +0530

    Fix Python packaging structure for pre-commit hooks

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='pre-commit-hooks-custom',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'check-added-large-files1=pre_commit_hooks.check_added_large_files1:main',
-        ],
-    },
-)
+setup()
