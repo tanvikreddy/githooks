2cbf278bc1359c784ce8050fd401ea2ad1445bf9
Santhosh Kumar Koppula <skoppula@microsoft.com>
2025-05-08 21:48:41 +0530

    T

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from __future__ import annotations
+from setuptools import setup, find_packages
 
-from setuptools import setup
-setup()
+setup(
+    name='pre-commit-hooks-custom',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'check-added-large-files1=pre_commit_hooks.check_added_large_files1:main',
+        ],
+    },
+)
